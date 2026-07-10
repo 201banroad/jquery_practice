32b314d08f9c0ad3e35f0ed5574058f187cc10f4
201banroad <hiroto01021202@icloud.com>
2026-07-10 14:13:33 +0900

    メソッドを切り出して整理

--- javascript/login.js
+++ javascript/login.js
@@ -1,40 +1,55 @@
 // 画面表示時のイベント
-$(function () {
-    
-    const openBtn = $("#openBtn");
-    const area = $("#subArea");
+$(function (){
+    toggleArea();
 
-    //エリア開閉
-    openBtn.click(function(){
-        area.slideToggle();
+    plusUser();
+
+    openForm();
+
+    closeForm();
+
+    formCheck();
+});
+
+//エリア開閉
+function toggleArea(){
+    $("#openBtn").click(function(){
+        $("#subArea").slideToggle();
     });
+}
 
-    //ユーザー追加
-    const addUser = $("#addUser");
+//行数を取得してユーザーの追加 
+function plusUser(){
     let userCount = $(".subBlk tbody tr").length +1;
-
-    addUser.click(function(){
-
+    $("#addUser").click(function(){
         $(".subBlk tbody").append(`
             <tr>
-				<td>test_user_${userCount}</td>
-				<td>テストユーザー${userCount}</td>
-    		</tr>
+                <td>test_user_${userCount}</td>
+                <td>テストユーザー${userCount}</td>
+            </tr>
         `);
         userCount++;
     });
+}
 
+// 新規登録ボタンクリックで新規登録フォームを表示
+function openForm(){
     $("#signup").click(function(){
         $(".modal-back").show();
         $(".dialogForm").show();
     });
+}
 
+// Xボタンクリックで新規登録フォームを非表示
+function closeForm(){
     $(".delete").click(function(){
         $(".modal-back").hide();
         $(".dialogForm").hide();
     });
+}
 
-    // フォームボタンが押されたら、未入力チェック
+// フォームのsubmitボタンが押されたら、未入力チェック
+function formCheck(){
     $("form").submit(function(e){
         e.preventDefault();
         const registId = $("#registId").val()
@@ -44,15 +59,14 @@ $(function () {
         || registPassword.trim() === "" 
         || registConfirm.trim() === "" ){
             alert("未入力の項目があります");
-        } else if(registPassword != registConfirm) {
+        } else if(registPassword != registConfirm){
             alert("パスワードと確認用パスワードの値が違います");
-        } else {
+        } else{
             alert("新規登録しました");
         }
-    })
+    });
+}
 
-
-});
 
 
 
