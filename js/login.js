b7ca93eab5b1aa4b616fc1238ec58878643f35ba
201banroad <hiroto01021202@icloud.com>
2026-07-12 22:36:41 +0900

    スペースやインデントの修正・新規登録アラートでた後にフォームを消す機能

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 // 画面表示時のイベント
-$(function(){
+$(function() {
     toggleUserArea();
 
     plusUser();
@@ -12,16 +12,16 @@ $(function(){
 });
 
 //エリア開閉
-function toggleUserArea(){
-    $("#openBtn").click(function(){
+function toggleUserArea() {
+    $("#openBtn").click(function() {
         $("#subArea").slideToggle();
     });
 }
 
 //行数を取得してユーザーの追加 
-function plusUser(){
+function plusUser() {
     let userCount = $(".subBlk tbody tr").length +1;
-    $("#addUser").click(function(){
+    $("#addUser").click(function() {
         $(".subBlk tbody").append(`
             <tr>
                 <td>test_user_${userCount}</td>
@@ -33,43 +33,45 @@ function plusUser(){
 }
 
 // 新規登録ボタンクリックで新規登録フォームを表示
-function openRegist(){
-    $("#signup").click(function(){
+function openRegist() {
+    $("#signup").click(function() {
         $(".modal-back").show();
         $(".dialogForm").show();
     });
 }
 
+// 新規登録フォームを非表示するメソッド
+function hideRegist() {
+    $(".modal-back").hide();
+    $(".dialogForm").hide();
+    $("#registForm")[0].reset();
+}
+
 // Xボタンクリックで新規登録フォームを非表示
-function closeRegist(){
-    $(".delete").click(function(){
-        $(".modal-back").hide();
-        $(".dialogForm").hide();
-        $("#registForm")[0].reset();
+function closeRegist() {
+    $(".delete").click(function() {
+        hideRegist();
     });
 }
 
 // フォームのsubmitボタンが押されたら、未入力チェック
-function checkRegist(){
+function checkRegist() {
     $("#registForm").submit(function(e){
         e.preventDefault();
         const registId = $("#registId").val()
         const registPassword = $("#registPassword").val()
         const registConfirm = $("#registConfirm").val()
-        if(registId.trim() === "" 
-        || registPassword.trim() === "" 
-        || registConfirm.trim() === "" ){
+        if (
+            registId.trim() === "" ||
+            registPassword.trim() === "" ||
+            registConfirm.trim() === ""
+        ){
             alert("未入力の項目があります");
-        } else if(registPassword != registConfirm){
+        } else if (registPassword != registConfirm){
             alert("パスワードと確認用パスワードの値が違います");
-        } else{
+        } else {
             alert("新規登録しました");
+            hideRegist();
         }
     });
 }
-
-
-
-
-
-    
